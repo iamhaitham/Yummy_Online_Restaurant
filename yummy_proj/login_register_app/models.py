7cbd1122f7048ada2256750611e600d52ced2be3
Mohammed Bayatena <www.cars2012@gmail.com>
2021-06-02 13:32:25 +0300

    Created models skeleton

--- yummy_proj/login_register_app/models.py
+++ yummy_proj/login_register_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from yummy_app.models import Cart
 
-# Create your models here.
+#TODO Fix imports
+
+
+class User(models.Model):
+    first_name=models.CharField(max_length=45)
+    last_name=models.CharField(max_length=45)
+    phone_number=models.CharField(max_length=45)
+    password=models.CharField(max_length=45)
+    city=models.CharField(max_length=45)
+    cart=models.OneToOneField(Cart,related_name="user",on_delete=models.CASCADE,primary_key=True)
+    #orders=
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
--- yummy_proj/yummy_app/models.py
+++ yummy_proj/yummy_app/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from login_register_app.models import User
 
-# Create your models here.
+#TODO Fix imports
+
+class Category(models.Model):
+    name = models.CharField(max_length=45)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    # dishes=
+    # restaurants=
+
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=45)
+    location = models.CharField(max_length=45)
+    phone_number = models.CharField(max_length=45)
+    website = models.CharField(max_length=255)
+    is_featured = models.BooleanField(default=False)
+    category = models.ManyToManyField(Category, related_name="restaurants")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=45)
+    description = models.CharField(max_length=255)
+    price = models.FloatField()
+    category = models.ForeignKey(Category, related_name="dishes", on_delete=models.CASCADED)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Location(models.Model):
+    city = models.CharField(max_length=45)
+    street = models.CharField(max_length=45)
+    home = models.CharField(max_length=45)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Order(models.Model):
+    quantity = models.IntegerField()
+    note = models.TextField()
+    grand_total = models.FloatField()
+    user = models.ForeignKey(User, related_name="orders", on_delete=models.CASCADE)
+    location = models.OneToOneField(Location, related_name="order",on_delete=models.CASCADE, primary_key=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Cart(models.Model):
+    dish=models.ManyToManyField(Dish,related_name="carts")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
