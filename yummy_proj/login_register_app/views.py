d1e97528c9cf17fafc4dcee141b41ed816d70508
Mohammed Bayatena <www.cars2012@gmail.com>
2021-06-02 11:00:38 +0300

    Added urls

--- yummy_proj/login_register_app/views.py
+++ yummy_proj/login_register_app/views.py
@@ -1,3 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+def loginregister(request):
+    return  HttpResponse("This is a palce holder for Login/Register app")
