d3fb654885f94e928c87e97db5975e7bbc3ab844
Mohammed Bayatena <www.cars2012@gmail.com>
2021-06-04 14:31:47 +0300

    Added Haitham Cart work

--- yummy_proj/yummy_app/views.py
+++ yummy_proj/yummy_app/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
+
 from . import models
+
 
 # Create your views here.
 
@@ -8,23 +10,24 @@ def home(request):
     return render(request, "home.html")
 
 
-def category(request,category):
-    context={
-        "categoryName":category,
-        "All_Dishes":models.getCategoryByName(category),
-        "Number":3,
+def category(request, category):
+    context = {
+        "categoryName": category,
+        "All_Dishes": models.getCategoryByName(category),
+        "Number": 3,
     }
-    return render(request,"categories.html",context)
+    return render(request, "categories.html", context)
+
 
 def cart(request):
     return render(request, "cart.html")
 
 
-def addToCart(request,category):
-    if request.method=="POST":
-        user_id=request.session["id"]
-        dishToAdd=request.POST["dishToAdd"]
-        models.addToCart(user_id,dishToAdd)
-        return redirect("/addToCart/"+category)
+def addToCart(request, category):
+    if request.method == "POST":
+        user_id = request.session["id"]
+        dishToAdd = request.POST["dishToAdd"]
+        models.addToCart(user_id, dishToAdd)
+        return redirect("/addToCart/" + category)
     else:
         return HttpResponse("You aren't allowed to manually modify the URL!")
